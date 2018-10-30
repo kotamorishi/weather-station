0a6085fdf2430ec775d585671e2f4db5a4da0c51
Kota <kouta.morishita@gmail.com>
2018-10-30 13:37:22 -0400

    Add download script

---
+++ download.sh
@@ -0,0 +1,4 @@
+wget -O current-data.json 'http://api.openweathermap.org/data/2.5/weather?id=6167865&&units=metric&appid=TYPE_YOUR_API_KEY_HERE'
+wget -O forecast-data.json 'http://api.openweathermap.org/data/2.5/forecast?id=6167865&units=metric&appid=TYPE_YOUR_API_KEY_HERE'
+
+
