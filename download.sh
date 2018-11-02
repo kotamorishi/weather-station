e7a47dba2061f15a2d5904bb5bb2b114c6e7e88a
Kota <44553544+kotamorishi@users.noreply.github.com>
2018-11-02 13:22:24 -0400

    fixed to easier to change key and location.
    tiny improvement.

--- download.sh
+++ download.sh
@@ -4,9 +4,12 @@
 # Tokyo : 1850147
 # Osaka : 1853909
 #
-# Change appid - get your API KEY from https://openweathermap.org 
+# Change appid - get it from https://openweathermap.org 
 #
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-wget -O ${SCRIPTPATH}/current-data.json 'http://api.openweathermap.org/data/2.5/weather?id=6167865&&units=metric&appid=TYPE_YOUR_API_KEY_HERE'
-wget -O ${SCRIPTPATH}/forecast-data.json 'http://api.openweathermap.org/data/2.5/forecast?id=6167865&units=metric&appid=TYPE_YOUR_API_KEY_HERE'
+API_KEY="TYPE_YOUR_API_KEY_HERE"
+WEATHER_ID=6167865
 
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+wget -O ${SCRIPTPATH}/current-data.json "http://api.openweathermap.org/data/2.5/weather?id=$WEATHER_ID&&units=metric&appid=$API_KEY"
+wget -O ${SCRIPTPATH}/forecast-data.json "http://api.openweathermap.org/data/2.5/forecast?id=$WEATHER_ID&units=metric&appid=$API_KEY"
